720b9b1de8e2c2402c9db55406aa7e277c49912d
cg14823 <19charles96@gmail.com>
2016-12-06 12:05:29 +0000

    small error on turning rebound to a kernel

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -709,7 +709,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
   checkError(err, "creating accelerate_flow kernel", __LINE__);
   ocl->propagate = clCreateKernel(ocl->program, "propagate", &err);
   checkError(err, "creating propagate kernel", __LINE__);
-  ocl->propagate = clCreateKernel(ocl->program, "rebound", &err);
+  ocl->rebound = clCreateKernel(ocl->program, "rebound", &err);
   checkError(err, "creating rebound kernel", __LINE__);
 
   // Allocate OpenCL buffers
